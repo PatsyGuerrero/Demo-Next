34574f601e978b9ac5b1c84f41f0a636c10a0ade
Patsy Guerrero <pguerrero3@bancoguayaquil.com>
2024-09-24 21:17:05 -0500

    funcionalidad para safari

--- src/app/components/productCard.jsx
+++ src/app/components/productCard.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react"
+import React, { useRef, useState, useEffect } from "react";
 import "./productCard.scss"
 // import parse from "html-react-parser"
 import Titles from "./titles"
@@ -10,6 +10,15 @@ import Image from "../components/image"
 // import { InViewObserver } from "../../../hooks/useInView"
 // import ButtonPrimary from "../../../components/buttons/buttonPrimary"
 import { useImageHook } from "../hooks/useImageHook"
+
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
+
+const mainVideo =
+  "https://videos.ctfassets.net/jhuukrkt1w7q/3najyQSTg2wIUsGxVm7L4z/7c931a401f77953a24cc701eb57b2830/metas.mp4";
+
 
 const ProductCard = ({ data }) => {
   switch (true) {
@@ -34,6 +43,43 @@ const ProductCardLayout = ({ children }) => {
 
 const ProductCardFirst = ({ data }) => {
   const imageData = useImageHook({ data });
+  const videoParentRef = useRef();
+  const [shouldUseImage, setShouldUseImage] = useState(false);
+
+  useEffect(() => {
+    // check if user agent is safari and we have the ref to the container <div />
+    if (isSafari() && videoParentRef.current) {
+      // obtain reference to the video element
+      const player = videoParentRef.current.children[0];
+
+      // if the reference to video player has been obtained
+      if (player) {
+        // set the video attributes using javascript as per the
+        // webkit Policy
+        player.controls = false;
+        player.playsinline = true;
+        player.muted = true;
+        player.setAttribute("muted", ""); // leave no stones unturned :)
+        player.autoplay = true;
+
+        // Let's wait for an event loop tick and be async.
+        setTimeout(() => {
+          // player.play() might return a promise but it's not guaranteed crossbrowser.
+          const promise = player.play();
+          // let's play safe to ensure that if we do have a promise
+          if (promise.then) {
+            promise
+              .then(() => {})
+              .catch(() => {
+                // if promise fails, hide the video and fallback to <img> tag
+                videoParentRef.current.style.display = "none";
+                setShouldUseImage(true);
+              });
+          }
+        }, 0);
+      }
+    }
+  }, []);
 
   console.log(imageData, 'imageData')
   const InformationSection = ({ data }) => {
@@ -48,15 +94,37 @@ const ProductCardFirst = ({ data }) => {
   }
 
   const ImageSection = ({ data}) => {
-    return (
-      <div id="image" className="cardContentPrimary__section">
-        <video width="576" height="300" autoplay="autoplay" playsinline aria-hidden="true" muted>
-          <source src="https://videos.ctfassets.net/jhuukrkt1w7q/3najyQSTg2wIUsGxVm7L4z/7c931a401f77953a24cc701eb57b2830/metas.mp4"/>
-            Your browser does not support the video tag.
-        </video>
-          {/* { <Image data={data} className="cardContentPrimary__image" /> } */}
-      </div>
-    )
+    // return (
+    //   <div id="image" className="cardContentPrimary__section">
+    //     <video width="576" height="300" autoplay="autoplay" playsinline aria-hidden="true" muted>
+    //       <source src="https://videos.ctfassets.net/jhuukrkt1w7q/3najyQSTg2wIUsGxVm7L4z/7c931a401f77953a24cc701eb57b2830/metas.mp4"/>
+    //         Your browser does not support the video tag.
+    //     </video>
+    //       {/* { <Image data={data} className="cardContentPrimary__image" /> } */}
+    //   </div>
+    // )
+
+    return shouldUseImage ? (
+      <img src={mainVideo} alt="Muted Video" />
+    ) : (
+      <div
+        ref={videoParentRef}
+        dangerouslySetInnerHTML={{
+          __html: `
+          <video
+            width="576" 
+            height="300"
+            loop
+            muted
+            autoplay
+            playsinline
+            preload="metadata"
+          >
+          <source src="${mainVideo}" type="video/mp4" />
+          </video>`
+        }}
+      />
+    );
   }
 
   if (data.left) {
